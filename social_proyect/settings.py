08b0b843451b5a176609ade1a77cce99142428b5
Juan Valencia <juanda.vid1996@hotmail.com>
2022-05-01 04:11:38 -0500

    avance #1

--- social_proyect/settings.py
+++ social_proyect/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'twitter.apps.TwitterConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Anexamos el redirect del login al home
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL= 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- twitter/views.py
+++ twitter/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Profile, Post
-
+from .forms import userRegisterForm, PostForm
+from django.contrib.auth.models import User
 
 def home(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user # asignamos el id del usuario en sesión
+            post.save()
+            return redirect('home')
+    else: 
+        form = PostForm()
+    
+    context = {
+        'posts': posts, 
+        'form': form,
+    }
     return render(request, 'twitter/newsfeed.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = userRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form = userRegisterForm()
+            
+    context={'form': form}
+    return render(request, 'twitter/register.html', context)
+
+def delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return redirect('home')
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    posts = user.posts.all()
+    context = {'user': user, 'posts': posts}
+    return render(request, 'twitter/profile.hmtl', context)
+
+# https://www.youtube.com/watch?v=06aDhOwqvfY&ab_channel=MundoPython encontrar solución a la linea 45
