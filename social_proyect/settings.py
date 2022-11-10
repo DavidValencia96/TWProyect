4f9e67f56e5f322b3e28ac8e39aec98da2d49177
Juan Valencia <juanda.vid1996@hotmail.com>
2022-11-09 23:30:05 -0500

    type changed

--- social_proyect/settings.py
+++ social_proyect/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware' # add line
+    # 'whitenoise.middleware.WhiteNoiseMiddleware' # add line
 ]
 
 WHITENOISE_MIMETYPES = {
