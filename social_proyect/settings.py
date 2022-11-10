a40addc53082173e1dbe2ef8ee741248e2b98947
Juan Valencia <juanda.vid1996@hotmail.com>
2022-11-09 23:05:32 -0500

    add mimetypes setting.py

--- social_proyect/settings.py
+++ social_proyect/settings.py
@@ -64,7 +64,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware' # add line
 ]
+
+WHITENOISE_MIMETYPES = {
+    '.xls':'application/xml'
+}
 
 ROOT_URLCONF = 'social_proyect.urls'
 
